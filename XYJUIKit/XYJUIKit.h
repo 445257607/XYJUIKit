bc62357aa8b63df486f183d805089fa9d324b23b
XiaoYingjun <445257607@qq.com>
2021-02-24 21:04:03 +0800

    创建空项目

---
+++ XYJUIKit/XYJUIKit.h
@@ -0,0 +1,18 @@
+//
+//  XYJUIKit.h
+//  XYJUIKit
+//
+//  Created by 肖迎军 on 2021/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XYJUIKit.
+FOUNDATION_EXPORT double XYJUIKitVersionNumber;
+
+//! Project version string for XYJUIKit.
+FOUNDATION_EXPORT const unsigned char XYJUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYJUIKit/PublicHeader.h>
+
+
